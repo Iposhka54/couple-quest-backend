616befd8cadaaff16b5d7db10867c71482b169d1
Iposhka54 <yaroslavkusb@gmail.com>
2026-03-30 19:37:38 +0300

    fix: add additional annotations for validate input data

--- auth-service/src/main/java/ru/iposhka/dto/request/AcceptInviteRequestDto.java
+++
@@ -1,8 +0,0 @@
-package ru.iposhka.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record AcceptInviteRequestDto(
-        @NotBlank(message = "Токен приглашения обязателен")
-        String token
-) {}
